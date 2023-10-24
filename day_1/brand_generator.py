72015d54e9989b1eccab30e710104886507a316a
vhmi <vhmizq@gmail.com>
2023-10-23 21:39:23 -0600

    add still second day

---
+++ day_1/brand_generator.py
@@ -0,0 +1,3 @@
+question_a = input('What"s your fav ice cream flavor?')
+question_b = input('When did you born?')
+print('Tne new brand is:',  question_b + question_a)
---
+++ day_2/format_strings.py
@@ -0,0 +1,14 @@
+power = 1900
+name = 'raven'
+phrase = 'My super hero is {} and has {} of power'.format(name, power)
+print(phrase)
+
+color = 'green'
+legs = 2
+
+phrase = f'He is {color} and has {legs} legs'
+print(phrase)
+
+
+
+
