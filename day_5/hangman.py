399de581502fe8c06921a2be3c75f4b3465df632
Victor Izquierdo <vhmizq@gmail.com>
2023-11-09 23:43:13 -0600

    add day 4

--- day_5/hangman.py
+++ day_5/hangman.py
@@ -1,23 +1,36 @@
 from random import choice
+chars = ''
 
-words = ['movies', 'car', 'lolipoop', 'smartband', 'developer']
 
-select_word = choice(words)
-print(select_word)
+def check_letter(word_, life_):
+    has_letter = ask in word_
+    if not has_letter:
+        life_ -= 1
+        print(f'''Wrong! 
+        Lost one life! 
+        lifes: {life_}''')
+
+
+def generate_spaces(word_):
+    length_spaces = len(word_)
+    spaces_char = '_' * length_spaces
+    chars = spaces_char
+    print(spaces_char)
+
+def replace_spaces(spaces, char):
+    chars.replace('_', char)
+
 
 play = True
+lifes = 6
+words = ['movies', 'car', 'lolipoop', 'smartband', 'developer']
+select_word = choice(words)
 
-def check_letter():
-    lifes = 6
-    has_letter = ask in select_word
-    if not has_letter:
-        lifes -= 1
-    print(f'lifes: {lifes}')
-def change_spaces():
-
+print('Welcome to Hangman. Guess the word before your tries run out')
+generate_spaces(select_word)
+print(select_word)
 
 while play:
     ask = input('Enter a guess letter: ').lower()
-    check_letter()
-
+    check_letter(select_word, lifes)
 
